3db747c9bb49ce390e87242ff9fe9a547c7e03a8
Brett Gillund <brettgillund2@gmail.com>
2022-07-18 23:00:06 -0500

    adding questions and creating readme file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const genMarkDown = require('./Develop/utils/generateMarkdown');
+
+
 // TODO: Create an array of questions for user input
 const questions = ['What would you like the title of the prokject to be?',
     'Please provide a description of the applicaiton.',
@@ -13,6 +16,9 @@ const questions = ['What would you like the title of the prokject to be?',
     'Please provide your GitHub link',
     'Please provide your email adress.'];
 
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
     inquirer.prompt([
         {
             name: 'title',
@@ -31,8 +37,10 @@ const questions = ['What would you like the title of the prokject to be?',
             message: 'Please provide intended usage'
         },
         {
+            type: 'list',
             name: 'license',
-            message: 'Please choose license type:'
+            message: 'Please choose license type:',
+            choices: ['', '', '', '', 'none']
         },
         {
             name: 'contributors',
@@ -50,15 +58,45 @@ const questions = ['What would you like the title of the prokject to be?',
             name: 'gitHub',
             message: 'What is your GitHub profile URL?'
         }
-    ]).then((data) => {
-        fs.writeFile(`${data.title}.md`, `${data}`, (error) =>
+    ]).then((data) =>
+        `# Title
+
+        ${data.title}
+        
+        # Description
+        
+        ${data.description}
+        
+        # Installation
+        
+        ${data.installation}
+        
+        # Usage
+        
+        ${data.usage}
+        
+        # License
+        
+        ${data.license}
+        
+        # Contributors
+        
+        ${data.contributors}
+        
+        # Test Data
+        
+        ${data.testData}
+        
+        # Questions
+        
+        ${data.email}        
+        ${data.gitHub}`
+
+    ).then((fileName) => {
+        fs.writeFile(`${fileName.title}.md`, `${fileName}`, (error) =>
         error ? console.error(error) : console.log('Success!'))
     });
-
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+}
 
 // TODO: Create a function to initialize app
 function init() {}
