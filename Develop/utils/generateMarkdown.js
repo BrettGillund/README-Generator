359ea5a283dbd2dbc4713412a082e6ecc5d66368
Brett Gillund <brettgillund2@gmail.com>
2022-08-21 17:49:26 -0500

    final commit before resubmission

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,99 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+var licenseBadge;
+var licenseLink;
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+// create an if statement to choose a badge url depending on their input. 
+  if(data.license === 'MIT License') {
+    return licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
 
-// TODO: Create a function that returns the license link
+  }else if(data.license === 'Mozilla License') {
+    return licenseBadge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]';
+
+
+  }else if(data.license === 'Apache License') {
+    return licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]';
+
+
+  }else if(data.license === 'GNU License') {
+    return licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+
+
+  }else if(data.license === 'none') {
+    return '';
+  };
+};
+
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+// create a list of if statements with the license badge links.
+if(data.license === 'MIT License') {
+  return licenseLink = 'https://opensource.org/licenses/MIT';
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+}else if(data.license === 'Mozilla License') {
+  return licenseLink = 'https://opensource.org/licenses/MPL-2.0';
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+}else if(data.license === 'Apache License') {
+  return licenseLink = 'https://opensource.org/licenses/Apache-2.0';
+
+
+}else if(data.license === 'GNU License') {
+  return licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
+
+
+}else if(data.license === 'none') {
+  return '';
+};
 }
+
+
+function generateMarkdown(data) {
+  renderLicenseBadge(data);
+  renderLicenseLink(data);
+  return `# ${data.title}
+    
+
+  ${licenseBadge}
+\n
+## Description
+  
+  ${data.description}
+\n
+## Table of Contents
+  
+1. Installation.
+2. Usage.
+3. License.
+4. Contributors.
+5. Test Data.
+6. Questions and Contact.
+\n 
+## Installation
+  
+  ${data.installation}
+\n
+## Usage
+  
+  ${data.usage}
+\n  
+## License
+  
+  ${data.license}
+  ${licenseBadge}
+  ${licenseLink}
+\n       \n
+## Contributors
+  
+  ${data.contributors}
+\n  
+## Test Data
+  
+  ${data.testData}
+\n  
+## Questions and Contact
+  
+  ${data.email}        
+  ${data.gitHub}`
+};
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const genMarkDown = require('./Develop/utils/generateMarkdown');
 
 
+
 // TODO: Create an array of questions for user input
 const questions = ['What would you like the title of the prokject to be?',
     'Please provide a description of the applicaiton.',
-    'Please provide a table of contents.',
     'Please provide installation requirements',
     'Please provide intended usage',
     'Please choose license type:',
@@ -16,104 +16,81 @@ const questions = ['What would you like the title of the prokject to be?',
     'Please provide your GitHub link',
     'Please provide your email adress.'];
 
-// prompts for the user to fill in.
+
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileData) {
+     // the format of the written README document.
+     // creates the file and sets up the user in case there is an error.
+        fs.writeFile(`README.md`, fileData, (error) =>
+        error ? console.error(error) : console.log('Success!'));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    // prompts for the user to fill in.
     inquirer.prompt([
         {
+            type: 'input',
             name: 'title',
-            message: `${questions[0]}`
+            message: `${questions[0]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the title section.'}},
         },
         {
             name: 'description',
             message: `${questions[1]}`
         },
         {
-            name: 'tableOfContents',
-            message: `${questions[2]}`
-        },
-        {
+            type: 'input',
             name: 'installation',
-            message: `${questions[3]}`
+            message: `${questions[2]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the installation section.'}},
+
         },
         {
+            type: 'input',
             name: 'usage',
-            message: `${questions[4]}`
+            message: `${questions[3]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the usage section.'}},
+
         },
         {
             type: 'list',
             name: 'license',
-            message: `${questions[5]}`,
-            choices: ['', '', '', '', 'none']
+            message: `${questions[4]}`,
+            choices: ['MIT License', 'Mozilla License', 'Apache License', 'GNU License', 'none'],
         },
         {
+            type: 'input',
             name: 'contributors',
-            message: `${questions[6]}`
+            message: `${questions[5]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the contributors section.'}},
         },
         {
+            type: 'input',
             name: 'testData',
-            message: `${questions[7]}`
+            message: `${questions[6]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the test data section.'}},
         },
         {
+            type: 'input',
             name: 'email',
-            message: `${questions[8]}`
+            message: `${questions[7]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the email section.'}},
+
         },
         {
+            type: 'input',
             name: 'gitHub',
-            message: `${questions[9]}`
+            message: `${questions[8]}`,
+            validate: (value)=>{ if(value){return true} else {return 'Please enter a value in the GitHub section.'}},
+
         }
-    ]).then((data) => // the format of the written README document.
-        `# Title
+    ]).then(genMarkDown).then(writeToFile);
 
-        ${data.title}
-        
-        ## Description
-        
-        ${data.description}
-
-        ## Table of Contents
-        
-        ${data.tableOfContents}
-        
-        ## Installation
-        
-        ${data.installation}
-        
-        ## Usage
-        
-        ${data.usage}
-        
-        ## License
-        
-        ${data.license}
-        
-        ## Contributors
-        
-        ${data.contributors}
-        
-        ## Test Data
-        
-        ${data.testData}
-        
-        ## Questions
-        
-        ${data.email}        
-        ${data.gitHub}`
-
-    ).then((fileName) => { // creates the file and sets up the user in case there is an error.
-        fs.writeFile(`README.md`, `${fileName}`, (error) =>
-        error ? console.error(error) : console.log('Success!'))
-    });
-
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    // what do they mean by initalizing the application?
-    writeToFile;
 }
 
 // Function call to initialize app
